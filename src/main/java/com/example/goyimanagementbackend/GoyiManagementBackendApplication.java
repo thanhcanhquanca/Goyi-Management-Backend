060b1bb48424ad58814aa8974814879737cb1e59
thanhcanhquanca <chuanhquan89@gmail.com>
2025-05-30 21:19:53 +0700

    init first

---
+++ src/main/java/com/example/goyimanagementbackend/GoyiManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.goyimanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoyiManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoyiManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/goyimanagementbackend/GoyiManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.goyimanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoyiManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
